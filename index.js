14af8885b9236d64550b6d8ed0fa8d710d1d81b1
ChrisVaz1995 <christopher.vazquez2015@gmail.com>
2021-08-18 14:58:10 -0500

    updated js file

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-const addMovies = (e) => {
-    document.querySelector('inputField');
+let message = document.querySelector('#message');
+
+const addMovie = (e) => {
+e.preventDefault();
+
+let inputField =  document.querySelector('input');
+console.log(inputField.value);
+const movie = document.createElement('li');
+const movieTitle = document.createElement('span');
+const deleteBtn = document.createElement('button');
+
+document.querySelector('ul').appendChild(movie);
+movieTitle.textContent = inputField.value;
+movieTitle.addEventListener('click', crossOffMovie);
+movie.appendChild(movieTitle);
+deleteBtn.textContent = 'X';
+deleteBtn.addEventListener('click', deleteMovie);
+movie.appendChild(deleteBtn);
+
+inputField.value = '';
+}
+
+document.querySelector('form').addEventListener('submit', addMovie);
+
+const deleteMovie = (e) => {
+    e.target.parentNode.remove();
+    message.textContent = 'Movie Deleted!'
+}
+
+const crossOffMovie = (e) => {
+    e.target.classList.toggle('checked');
+    if(e.target.classList.contains('checked') === true) {
+        message.textContent = 'Movie watched';
+    } else {
+        message.textContent = 'Movie added back'    
+    }
 }
